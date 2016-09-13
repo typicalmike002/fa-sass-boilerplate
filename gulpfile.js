328a7fbf85dfb11f60b4430481851d20d57d346a
typicalmike002 <townsendm105@gmail.com>
2016-09-13 15:08:59 -0400

    Added mobile optimizations

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,10 @@
 var gulp = require('gulp'),
-    os   = require('os');
+    ip   = require('ip');
 
 
 
-/**
- * Matchdep
- * 
- * - Automatically loads all package.json developer dependencies prefixed with 'gulp-'.
- */
 
+// Loads all gulp modules prefexed as 'gulp-' inside the package.json file as variables.
 require('matchdep').filterDev('gulp*').forEach(function( module ) {
     var module_name     = module.replace(/^gulp-/, '').replace(/-/, '');
     global[module_name] = require( module );
@@ -16,42 +12,61 @@ require('matchdep').filterDev('gulp*').forEach(function( module ) {
 
 
 
-/**
- * ipAddress
- *
- * - Gets the current ip address of the computer for running livereload.
- *
- * - Only works on wifi networks.
- */
 
-var ipAddress = (function(){
-    var thisNetwork = os.networkInterfaces(),
-        myIp        = null;
-    thisNetwork['Wi-Fi'].forEach(function(details){
-        if (details.family === 'IPv4') {
-            myIp = details.address;
-        }
-    });
-    return myIp;
-}());
-
-
-
+// Launches a livereload socket and binds the reload:chrome task
+// to html and sass files inside the project:
 gulp.task('watch', function(){
 
     // Adds livereload support:
     connect.server({
-        host: ipAddress,
-        port: 8888,
-        livereload: true
+        host: ip.address(),
+        port: 8080,
+        livereload: true,
+        debug: true
     });
 
-    gulp.watch('html/index.html', ['compile:all']);
-    gulp.watch('css/sass/**/*.scss', ['compile:all']);
+    gulp.watch('html/index.html', ['reload:chrome']);
+    gulp.watch('css/sass/**/*.scss', ['reload:chrome']);
 });
 
 
 
+
+// Reloads a chrome browser using the livereload extension:
+gulp.task('reload:chrome', ['clean:css'], function(){
+    return gulp.src('./')
+        .pipe(connect.reload())
+});
+
+
+
+
+// Removes unused css from all .css files:
+gulp.task('clean:css', ['compile:html'], function(){
+    return gulp.src('css/*.css')
+        .pipe(uncss({
+            html: ['index.html']
+        }))
+        .pipe(gulp.dest('./css'))
+});
+
+
+
+
+// Compiles critical css inside the html/index.html 
+// file and saves the results to the root directory:
+gulp.task('compile:html', ['compile:css'], function(){
+    return gulp.src('./html/index.html')
+        .pipe(inlinesource({
+            compress: false
+        }))
+        .pipe(gulp.dest('./'))
+});
+
+
+
+
+// Compiles .scss files to .css and .min.css:
 gulp.task('compile:css', function(){
     return gulp.src([
             '!css/sass/font-awesome-4.6.3/**/*.scss',
@@ -66,15 +81,19 @@ gulp.task('compile:css', function(){
             environment: 'development'
         }))
 
-        // Pass an error handler to compass:
+        // Processes errors thrown by compass:
         .on('error', function(err){
             console.log(err);
         })
 
-        // While still compiling, add cross browser prefixes 
-        // and combine all media queries:
+        // Runs postcss plugins:
         .pipe(postcss([
+
+            // Autoprefixer will inject all browser prefixes 
+            // to the style tags that require them:
             require('autoprefixer')({ browsers: ['last 2 versions'] }),
+
+            // Combines media queries with the same width:
             require('css-mqpacker')
         ]))
 
@@ -93,29 +112,6 @@ gulp.task('compile:css', function(){
 
         // Saves the minified results:
         .pipe(gulp.dest('./css'))
-});
-
-
-gulp.task('compile:html', ['compile:css'], function(){
-    return gulp.src('./html/index.html')
-        .pipe(inlinesource({
-            compress: false
-        }))
-        .pipe(gulp.dest('./'))
-});
-
-
-
-// Removes unused css from all .css files:
-gulp.task('compile:all', ['compile:html'], function(){
-    return gulp.src('css/*.css')
-        .pipe(uncss({
-            html: ['html/index.html']
-        }))
-        .pipe(gulp.dest('./css'))
-
-        // Reload Window:
-        .pipe(connect.reload())
 });
 
 
